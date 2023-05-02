934202489bca7be33e30d5b5d0201ffe1eb648e9
NoMoreNap <vv2trash@gmail.com>
2023-05-02 10:24:14 +0300

    ts edit

--- index.d.ts
+++ index.d.ts
@@ -14,7 +14,25 @@ declare module '*templateEngine' {
     export default function templateEngine(elem: object): Node;
 }
 
+interface screens {
+    [selector: string]: () => void;
+}
+
+interface blocks {
+    [selector: string]: (parent: HTMLElement) => void;
+    
+}
+
+type appType = {
+    blocks: blocks;
+    screens: screens ;
+    renderScreen: (screen: string) => void;
+    renderBlock: (parent: HTMLElement, blocks: string) => void;
+    timers: Array<()=>void>;
+    stopTimer: () => void;
+    alert: (text: string, time: number) => void;
+}
 
 declare interface Window {
-    application: any;
+    application: appType;
 }
